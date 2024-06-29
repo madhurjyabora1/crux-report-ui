5cd2281bcf049bac9125238a8eedeceeced82649
Madhurjya Bora <madhurjya@csuitecircle.com>
2024-06-30 00:44:39 +0530

    env

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,9 +2,12 @@ import axios from "axios";
 
 async function fetchCruxReport(url) {
   try {
-    const response = await axios.post("http://localhost:3001/api/crux-report", {
-      url,
-    });
+    const response = await axios.post(
+      `${process.env.REACT_APP_URL}/api/crux-report`,
+      {
+        url,
+      }
+    );
     return response.data;
   } catch (error) {
     throw error.response ? error.response.data : error;
